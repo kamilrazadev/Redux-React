792585f5506f2a3232aeaec138c359e7b04258a2
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-11-12 01:30:51 +0500

    chance fake API added

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import DeleteUser from "./DeleteUser";
-
+import { fakeUserData } from "../api";
+                                                                                                                                                                       
 const UserDetails = () => {
+  const addNewUser = (payload) => {
+    console.log(payload);
+  };
+
   return (
     <div className="content">
-      <div className="admin-table">
+      <div className="admin-table d-flex">
         <div className="admin-subtitle">List of User Details</div>
-        <button>Add New User</button>
+        <button onClick={() => addNewUser(fakeUserData())}>Add New User</button>
       </div>
       <ul>
         {/* <li>Hi</li>
